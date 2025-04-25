fc52bf4860f9e45997b86045da8e999e35114719
hoatien <22021144@vnu.edu.vn>
2025-04-25 12:15:43 +0700

    add:copilotkit

---
+++ ui/src/app/layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+import "./globals.css";
+ 
+import { ReactNode } from "react";
+import { CopilotKit } from "@copilotkit/react-core"; 
+ 
+export default function RootLayout({ children }: { children: ReactNode }) {
+    return (
+      <html lang="en">
+        <body> 
+          {/* Use the public api key you got from Copilot Cloud  */}
+          <CopilotKit publicApiKey="ck_pub_333f31a711d12cb6c23c332fd24fc11e"> 
+            {children}
+          </CopilotKit>
+        </body>
+      </html>
+    );
+}
---
+++ ui/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+ 
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotPopup } from "@copilotkit/react-ui"; 
+import "@copilotkit/react-ui/styles.css"; 
+ 
+export default function Home() {
+  return (
+    <>
+      <CopilotPopup />
+    </>
+  );
+}
